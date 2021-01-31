d97fba62e664aadd5e3bcfd4166fb1eefce05046
JJH <62762383+JJH47E@users.noreply.github.com>
2021-01-31 13:52:06 +0000

    Added guides

--- core/src/com/threecubed/auber/ui/MenuUI.java
+++ core/src/com/threecubed/auber/ui/MenuUI.java
@@ -32,10 +32,17 @@ public class MenuUI {
   Button easyDiffButton;
   Button hardDiffButton;
   Button loadButton;
+  Button nextButton;
+  Button lastButton;
+  Button startButton;
   OrthogonalTiledMapRenderer renderer;
-  Sprite instructions;
+  Sprite instructions_first;
+  Sprite instructions_second;
   Sprite title;
   SpriteBatch spriteBatch;
+
+  // Main menu screen counter (main, guide 1, guide 2)
+  int subScreen = 0;
 
   /**
    * Instantiate the screen with the {@link AuberGame} object. Set the title and button up to be
@@ -48,10 +55,17 @@ public class MenuUI {
 
     spriteBatch = new SpriteBatch();
 
-    instructions = game.atlas.createSprite("instructions");
+    instructions_first = game.atlas.createSprite("guide1");
+    instructions_first.setScale(1f);
+    instructions_first.setPosition(Gdx.graphics.getWidth() / 2 - instructions_first.getWidth() / 2,
+            Gdx.graphics.getHeight() / 2 - instructions_first.getHeight() / 2 + 50f);
+    instructions_second = game.atlas.createSprite("guide2");
+    instructions_second.setScale(1f);
+    instructions_second.setPosition(Gdx.graphics.getWidth() / 2 - instructions_second.getWidth() / 2,
+            Gdx.graphics.getHeight() / 2 - instructions_second.getHeight() / 2 + 50f);
     title = game.atlas.createSprite("auber_logo");
 
-    Runnable onPlayClick = new Runnable() {
+    Runnable onStartClick = new Runnable() {
       @Override
       public void run() {
         game.setScreen(new GameScreen(game, MenuScreen.difficulty));
@@ -59,9 +73,9 @@ public class MenuUI {
     };
 
     // Button sprites are now drawn from the bottom left rather than center
-    playButton = new Button(
-        new Vector2(0f, Gdx.graphics.getHeight() / 4),
-        1f, game.atlas.createSprite("playButton"), game.atlas.createSprite("playButtonPressed"), game, onPlayClick);
+    startButton = new Button(
+        new Vector2(Gdx.graphics.getWidth() / 2 - 200f, Gdx.graphics.getHeight() / 4 - 175f),
+        1f, game.atlas.createSprite("startButton"), game.atlas.createSprite("startButtonPressed"), game, onStartClick);
 
     Runnable onDiffClick = new Runnable() {
       @Override
@@ -90,25 +104,74 @@ public class MenuUI {
     loadButton = new Button(
             new Vector2(0f, Gdx.graphics.getHeight() / 4 - 125f),
             1f, game.atlas.createSprite("loadButton"), game.atlas.createSprite("loadButtonPressed"), game, onLoadClick);
+
+    Runnable onNextClick = new Runnable() {
+      @Override
+      public void run() {
+        nextScreen();
+      }
+    };
+
+    nextButton = new Button(
+            new Vector2(Gdx.graphics.getWidth() - 400f, Gdx.graphics.getHeight() / 4 - 175f),
+            1f, game.atlas.createSprite("nextButton"), game.atlas.createSprite("nextButtonPressed"), game, onNextClick);
+
+    playButton = new Button(
+            new Vector2(0f, Gdx.graphics.getHeight() / 4),
+            1f, game.atlas.createSprite("playButton"), game.atlas.createSprite("playButtonPressed"), game, onNextClick);
+
+    Runnable onLastClick = new Runnable() {
+      @Override
+      public void run() {
+        lastScreen();
+      }
+    };
+
+    lastButton = new Button(
+            new Vector2(0f, Gdx.graphics.getHeight() / 4 - 175f),
+            1f, game.atlas.createSprite("backButton"), game.atlas.createSprite("backButtonPressed"), game, onLastClick);
   }
 
   public void render(World world, SpriteBatch spriteBatch) {
 
     spriteBatch.begin();
 
-    title.setScale(0.5f);
-    title.setPosition(-200f, 600f);
-    title.draw(spriteBatch);
+    if (subScreen == 0) {
 
-    playButton.render(spriteBatch);
-    loadButton.render(spriteBatch);
-    if (MenuScreen.difficulty == Difficulties.Easy) {
-      easyDiffButton.render(spriteBatch);
+      title.setScale(0.5f);
+      title.setPosition(-200f, 600f);
+      title.draw(spriteBatch);
+
+      playButton.render(spriteBatch);
+      loadButton.render(spriteBatch);
+      if (MenuScreen.difficulty == Difficulties.Easy) {
+        easyDiffButton.render(spriteBatch);
+      } else {
+        hardDiffButton.render(spriteBatch);
+      }
+
+    }
+    else if (subScreen == 1) {
+      // draw guide 1
+      instructions_first.draw(spriteBatch);
+      nextButton.render(spriteBatch);
+      lastButton.render(spriteBatch);
     }
     else {
-      hardDiffButton.render(spriteBatch);
+      // draw guide 2
+      instructions_second.draw(spriteBatch);
+      lastButton.render(spriteBatch);
+      startButton.render(spriteBatch);
     }
 
     spriteBatch.end();
+  }
+
+  public void nextScreen() {
+    this.subScreen++;
+  }
+
+  public void lastScreen() {
+    this.subScreen--;
   }
 }
