8e1287cb8cf974a63302d41f680ded62e6b12b8d
JJH <62762383+JJH47E@users.noreply.github.com>
2021-02-07 17:40:27 +0000

    Update DifficultiesTest.java

--- desktop/test/com/threecubed/auber/DifficultiesTest.java
+++ desktop/test/com/threecubed/auber/DifficultiesTest.java
@@ -1,8 +1,5 @@
 package com.threecubed.auber;
 
-import com.badlogic.gdx.math.Vector2;
-import com.threecubed.auber.entities.Civilian;
-import com.threecubed.auber.screens.GameScreen;
 import com.threecubed.auber.ui.Difficulties;
 import de.tomgrill.gdxtesting.GdxTestRunner;
 import org.junit.Assert;
@@ -10,14 +7,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.mockito.Mockito.mock;
-
 @RunWith(GdxTestRunner.class)
 public class DifficultiesTest {
 
-    private AuberGame game;
-    private World world;
-    private GameScreen gs;
     private Difficulties diffEasy;
     private Difficulties diffHard;
 
