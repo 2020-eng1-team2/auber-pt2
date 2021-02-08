e713c9ba398badeaeacd8f4c1b9032df0f351725
JJH <62762383+JJH47E@users.noreply.github.com>
2021-02-08 13:45:01 +0000

    Final test

--- desktop/test/com/threecubed/auber/NpcTest.java
+++ desktop/test/com/threecubed/auber/NpcTest.java
@@ -3,6 +3,7 @@ package com.threecubed.auber;
 import com.badlogic.gdx.math.Vector2;
 import com.threecubed.auber.entities.Civilian;
 import com.threecubed.auber.entities.GameEntity;
+import com.threecubed.auber.entities.Infiltrator;
 import com.threecubed.auber.entities.Npc;
 import com.threecubed.auber.screens.GameScreen;
 import com.threecubed.auber.ui.Difficulties;
@@ -11,6 +12,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
 
 import static org.mockito.Mockito.mock;
 
@@ -53,5 +56,29 @@ public class NpcTest {
             }
         }
         Assert.assertEquals(24, counter);
+    }
+
+    @Test
+    public void civilianSpawnTest() throws ClassNotFoundException {
+        world.updateEntities();
+        ArrayList<Civilian> civilians = new ArrayList<>();
+        for (GameEntity ent : world.getEntities()) {
+            if (ent instanceof Civilian) {
+                civilians.add((Civilian) ent);
+            }
+        }
+        if (civilians.isEmpty()) {
+            // No infiltrators
+            throw new ClassNotFoundException("No Civilians on map");
+        }
+        for (Civilian civilian : civilians) {
+            for (Civilian civilian1 : civilians) {
+                if (civilian.position == civilian1.position && !(civilian.equals(civilian1))) {
+                    // Two infiltrators have spawned in the same position
+                    Assert.assertTrue(false);
+                }
+            }
+        }
+        Assert.assertTrue(true);
     }
 }
